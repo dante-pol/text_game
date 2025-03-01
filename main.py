07d3955152e6ecdcd4af62b0b528429dee2cb7bf
Dante Pol <dim.drvlg@gmail.com>
2025-03-01 20:20:23 +0300

    добавил функцию битвы с монстром милишником

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-# This is a sample Python script.
+def fight_monster_melee(player: list[int, int], monster: list[int, int]):
+    import random
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    player_hp = player[0]
+    player_damage = player[1]
 
+    monster_hp = monster[0]
+    monster_damage = monster[1]
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    has_first_go_player = True if random.randint(0, 100) > 60 else False
 
+    while (player_hp > 0) or (monster_hp > 0):
+        if has_first_go_player:
+            monster_hp -= player_damage
+            has_first_go_player = False
+        else:
+            player_hp -= monster_damage
+            has_first_go_player = True
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    if player_hp > 0:
+        return "Игрок одержал победу!"
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    if monster_hp > 0:
+        return "Монстр одержал победу!"
