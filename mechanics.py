f2e005f8bb2a8843d8d5c502d57c8fbdbd880bec
Dante Pol <dim.drvlg@gmail.com>
2025-03-01 21:00:25 +0300

    исправил и улучшил функию драки игрока и монстра

--- mechanics.py
+++ mechanics.py
@@ -1,27 +1,34 @@
+import time
+
+
 def fight_monster_melee(player: list[int, int], monster: list[str, int, int]):
     import random
 
     player_hp = player[0]
     player_damage = player[1]
 
-    monster_hp = monster[0]
-    monster_damage = monster[1]
+    monster_name = monster[0]
+    monster_hp = monster[1]
+    monster_damage = monster[2]
 
     has_first_go_player = True if random.randint(0, 100) > 60 else False
 
     while (player_hp > 0) or (monster_hp > 0):
         if has_first_go_player:
+            print("Герой атакует!")
             monster_hp -= player_damage
             has_first_go_player = False
         else:
+            print("Монстр атакует!")
             player_hp -= monster_damage
             has_first_go_player = True
+        time.sleep(0.1)
 
     if player_hp > 0:
         return "Игрок одержал победу!"
 
     if monster_hp > 0:
-        return "Монстр одержал победу!"
+        return f"Монстр {monster_name} одержал победу!"
 
 
 def loot_box(items: list[str, ...]) -> None or str:
