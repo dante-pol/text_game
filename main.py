945911c8e60da78a178aa9d552850f42f394c22d
Artem Klivniak <klivnaka11@gmail.com>
2025-03-01 12:32:53 -0500

    Сделал расчет урона и жизней при выборе разного снаряжения

--- main.py
+++ main.py
@@ -3,10 +3,56 @@ print("Приветствуем в нашей текстовой игре")
 
 print("Выберите свое снаряжение")
 
-weapon = int(input("Выбор оружия\n1.обычный меч\n2.пистолет\n3.посох мага"))
+damage_list = []
+health_list = []
 selection = []
+
+weapon = int(input("Выбор оружия\n1.обычный меч\n2.пистолет\n3.посох мага"))
 selection.append(weapon)
+
+def count_damage(weapon):
+    if weapon == 1:
+        damage = 7
+
+    if weapon == 2:
+        damage = 12
+
+    if weapon == 3:
+        damage = 5
+
+    return damage
+
 armor = int(input("Выбор брони\n1.Латная броня\n2.бронежилет\n3.роба мага"))
 selection.append(armor)
+
+def count_health(armor):
+    if armor == 1:
+        health = 100
+
+    if armor == 2:
+        health = 70
+
+    if armor == 3:
+        health = 40
+
+    return health
+
 skill = int(input("Выбор способности\n1.усиление физических показателей\n2.ускоренная перезарядка\n3.знание начальных заклинаний"))
 selection.append(skill)
+
+def count_bonus_damage(skill):
+    if skill == 1:
+        bonus_damage = 5
+
+    if skill == 2:
+        bonus_damage = 3
+
+    if skill == 3:
+        bonus_damage = 7
+
+    return bonus_damage
+
+sum_damage = count_damage(weapon) + count_bonus_damage(skill)
+damage_list.append(sum_damage)
+
+health_list.append(count_health(armor))
