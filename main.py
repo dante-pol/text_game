11824be1c29793f4f1cbd29d1e1f3319d1437148
Artem Klivniak <klivnaka11@gmail.com>
2025-03-01 12:58:38 -0500

    Создан файл enteties_factory для создания сущностей

--- main.py
+++ main.py
@@ -58,20 +58,3 @@ damage_list.append(sum_damage)
 
 health_list.append(count_health(armor))
 
-def create_players(health_list,damage_list):
-    player = [[health_list],[damage_list]]
-    return player
-
-def create_monster() -> list[str, int, int]:
-    import monstersconfigs
-    import random
-
-    name = monstersconfigs.NAMES[random.randint(0, monstersconfigs.NAMES)]
-    hp = monstersconfigs.HEAT_POINTS[random.randint(0, monstersconfigs.HEAT_POINTS)]
-    damage = monstersconfigs.DAMAGES[random.randint(0, monstersconfigs.DAMAGES)]
-
-    monster = [name, hp, damage]
-
-    return monster
-
-
