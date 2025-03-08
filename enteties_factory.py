aec3f9baaae2c7e75ede07c0f6da262992563042
Artem Klivniak <klivnaka11@gmail.com>
2025-03-07 20:54:32 -0500

    Создал функцию create_monster_ranged

--- enteties_factory.py
+++ enteties_factory.py
@@ -14,4 +14,15 @@ def create_monster() -> list[str, int, int]:
 
     return monster
 
+def create_ranged_monster():
+    import monstersconfigs
+    import random
+
+    name_ranged = monstersconfigs.NAMES_RANGED[random.randint(0, len(monstersconfigs.NAMES_RANGED)-1)]
+    hp_ranged = monstersconfigs.HEAT_POINTS_RANGED[random.randint(0, len(monstersconfigs.HEAT_POINTS_RANGED)-1)]
+    damage_ranged = monstersconfigs.DAMAGES_RANGED[random.randint(0, len(monstersconfigs.DAMAGES_RANGED)-1)]
+
+    ranged_monster = [name_ranged, hp_ranged, damage_ranged]
+
+    return ranged_monster
 
