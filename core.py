fe84fb19d94e260b19de99be35a6f13bdf000693
Artem Klivniak <klivnaka11@gmail.com>
2025-03-23 13:21:32 -0400

    Создал модуль roomconfigs для создания комнат и дополнил core

--- core.py
+++ core.py
@@ -1,15 +1,30 @@
-
+from mechanics import class_selection
+from enteties_factory import create_players
 def bootstrap():
-    # Hello for Player
-    # Main Menu: 1. New | 2. Continue | 3. Exit
-    pass
+    print("Приветствуем в нашей текстовой игре")
+    print("1. Новая игра\n2. Продолжить игру\n3. Выйти")
+
 
 def gameloop():
     # user input
     is_gaming = True
 
     while is_gaming:
-        pass
+        user_input = int(input(">>"))
+        if user_input == 3:
+            is_gaming = False
+            continue
+        if user_input == 1:
+            player_name = input("Как вас зовут >>")
+
+            player_class = class_selection()
+
+            player = create_players(player_name,player_class)
+
 
 def dispose():
-    pass
+    print("Пока!")
+
+
+
+
