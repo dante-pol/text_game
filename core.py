6f0092e9acd7ccb6d259c2f57eb6bfefc703bc76
Dmitry Rak <dmitry-rak@saros.games>
2025-03-23 19:16:03 +0300

    добавил пояснения к функция core

--- core.py
+++ core.py
@@ -1,10 +1,15 @@
 
+def bootstrap():
+    # Hello for Player
+    # Main Menu: 1. New | 2. Continue | 3. Exit
+    pass
 
-def game_loop():
-
+def gameloop():
+    # user input
     is_gaming = True
 
     while is_gaming:
         pass
 
-
+def dispose():
+    pass
