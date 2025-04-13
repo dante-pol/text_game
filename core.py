766961062dcbb6d9a89ef0820d2405b2fa87f500
Artem Klivniak <klivnaka11@gmail.com>
2025-04-13 12:38:29 -0400

    Добавил открытие этажей

--- core.py
+++ core.py
@@ -1,6 +1,7 @@
-from mechanics import class_selection
+from mechanics import *
 from enteties_factory import create_players
 from enteties_factory import create_stage
+from roomconfigs import *
 def bootstrap():
     print("Приветствуем в нашей текстовой игре")
     print("1. Новая игра\n2. Продолжить игру\n3. Выйти")
@@ -25,6 +26,29 @@ def gameloop():
             stages_list = []
         for i in range(0,stages,1):
             stages_list.append(create_stage(player))
+        print("Хотите ли вы начать свое приключение?(1-нет;2-да)")
+        user_input = int(input())
+        if user_input == 1:
+            is_gaming = False
+            continue
+        for stage in stages_list:#stage_list[[stage],[stage],[stage]]
+            print("открываем этаж...")
+            for room in stage:#stage[[]]
+                if room[0] == LOOTBOX:
+                    open_loot_box(player,room[1])
+
+                if room[0] == BOSS:
+                    fight_boss(player,room[1])
+
+                if room[0] == MELEE:
+                    fight_monster_melee(player,room[1])
+
+                if room[0] == RANGE:
+                    fight_monster_ranged(player,room[1])
+            print("Этаж пройден")
+
+                
+        
 
 def dispose():
     print("Пока!")
